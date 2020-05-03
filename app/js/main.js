65f86448e21718a63a30588d407bbba2a27309f5
vahan akopjanian <vahan.bclub@gmail.com>
2020-05-03 12:10:27 +0300

    added surf map with sliders and info-modals

--- app/js/main.js
+++ app/js/main.js
@@ -6,12 +6,30 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="" />',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="" />',
-    asNavFor: ".slider-dots",
+    asNavFor: ".slider-dots-header",
   });
 
-  $(".slider-dots").slick({
+  $(".slider-dots-header").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header__slider",
+  });
+
+  $(".surf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="" />',
+    asNavFor: ".slider-map",
+  });
+
+  $(".slider-map").slick({
+    slidesToShow: 8,
+    arrows: false,
+    slidesToScroll: 1,
+    asNavFor: ".surf-slider",
+    focusOnSelect: true,
   });
 });
